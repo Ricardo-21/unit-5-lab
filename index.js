29eb4a30a7bb7215471089e3342fb6ee096a484d
segunsub <segunsubair3@gmail.com>
2021-02-02 18:39:18 -0500

    update

--- index.js
+++ index.js
@@ -11,19 +11,39 @@ const midR = document.getElementById('midR');
 const bottomL = document.getElementById('bottomL');
 const bottomM = document.getElementById('bottomM');
 const bottomR = document.getElementById('bottomR');
-
+const row1 = document.getElementById('row1')
+const col1 = document.querySelectorAll(".col1")
 let currentP = 1;
 
+let col1str = ''
 function placeXO(event){
-    console.log(event.target)
+  if(event.target.innerText === "") { 
     if(currentP === 1){
+        if(event.target.parentNode.name == undefined) {
+            event.target.parentNode.name = "X"
+        } else{
+        event.target.parentNode.name += 'X'
+        }
+        col1.forEach(x => col1str += x.innerText)
         event.target.innerText = 'X'
         currentP = 2;
+        
     }
     else{
+        if(event.target.parentNode.name == undefined) {
+            event.target.parentNode.name = "O"
+        } else{
+        event.target.parentNode.name += 'O'
+        }
+        col1.forEach(x => col1str += x.innerText)
         event.target.innerText = 'O'
         currentP = 1;
     }
+}
+// event.target.parentNode.name += 'f'
+// console.log(event.target.parentNode.name)
+// console.log(row1)
+checkwinner(event)
 }
 
 topL.addEventListener('click', placeXO);
@@ -37,3 +57,10 @@ midR.addEventListener('click', placeXO);
 bottomL.addEventListener('click', placeXO);
 bottomM.addEventListener('click', placeXO);
 bottomR.addEventListener('click', placeXO);
+ 
+function checkwinner(x) {
+    console.log(col1str)
+    if (x.target.parentNode.name === "XXX") {
+        alert('x won')
+    } 
+}
