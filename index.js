acd78bd1bdbae2293f3595be08f8591b8676f403
segunsub <segunsubair3@gmail.com>
2021-02-02 16:50:37 -0500

    cleaning

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const gameBox = document.createElement('div');
-gameBox.style.backgroundColor = 'blue';
-gameBox.style.width = '600px';
-gameBox.style.height = '600px';
-gameBox.style.position = 'relative';
+const gameBox = document.getElementById('gameBox');
 
-const topL = document.createElement('div');
-gameBox.append(topL);
-// topL.style.border = '10px';
-// topL.style.borderColor = 'black';
-// topL.style.borderStyle = 'dashed';
-topL.style.backgroundColor = 'red';
-topL.style.top = '0px';
-topL.style.left = '0px';
-topL.style.width = '200px';
-topL.style.height = '200px';
-topL.style.position = 'absolute';
+const topL = document.getElementById('topL');
 
-const body = document.querySelector('body');
-body.append(gameBox);
